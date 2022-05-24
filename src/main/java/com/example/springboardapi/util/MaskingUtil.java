94430946da1b1ddefd9d9a48d1f2cc7735839727
jepark0121 <jepark0121@gmail.com>
2022-05-24 11:01:24 +0900

    fix: StringBuilder로 마스킹 처리 변경.

--- src/main/java/com/example/springboardapi/util/MaskingUtil.java
+++ src/main/java/com/example/springboardapi/util/MaskingUtil.java
@@ -5,30 +5,29 @@ public class MaskingUtil {
      * 이름 마스킹 처리
     */
     public static String nameMasking(String name) {
-        String masked = "";
-        String middleName = "";
-
         if(name == null || name.trim().isEmpty()) {
             return "";
         }
 
-        // 이름이 3자 이상이면
+        int startIndex = 1;
+        int endIndex = 0;
         if(name.trim().length() > 2) {
-            middleName = name.substring(1, name.trim().length()-1);
+            endIndex = name.trim().length() - 1;
         } else {
-            middleName = name.substring(1);
+            endIndex = name.trim().length();
         }
 
-        masked = name.substring(0, 1);
-        for(int i=0; i<middleName.length(); i++){
-            masked += "*";
-            if(i == middleName.length() - 1){
-                if(name.trim().length() > 2) {
-                    masked += name.substring(name.trim().length()-1, name.trim().length());
-                }
+        // String은 변경 불가능한 문자열을 생성하지만 StringBuilder는 변경 가능한 문자열을 만들어 주기 때문에, String을 합치는 작업 시 하나의 대안이 될 수 있다
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] chars = name.trim().toCharArray();
+        for(int i=0; i<name.trim().length(); i++) {
+            if(i >= startIndex && i < endIndex) {
+                stringBuilder.append("*");
+            } else {
+                stringBuilder.append(chars[i]);
             }
         }
 
-        return masked;
+        return stringBuilder.toString();
     }
 }
