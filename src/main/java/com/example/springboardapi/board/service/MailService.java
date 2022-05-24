69d633989afcb870dcdc785281c8c392ae7d9215
jepark0121 <jepark0121@gmail.com>
2022-05-24 10:28:38 +0900

    fix: Date -> Joda Time으로 변경.

--- src/main/java/com/example/springboardapi/board/service/MailService.java
+++ src/main/java/com/example/springboardapi/board/service/MailService.java
@@ -1,13 +1,15 @@
 package com.example.springboardapi.board.service;
 
 import lombok.extern.log4j.Log4j2;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.internet.MimeMessage;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 @Log4j2
 @Service
@@ -21,10 +23,11 @@ public class MailService {
     }
 
     public void sendMail(String title) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter dateTime = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         String subject = "[게시글 삭제] “" + title + "” 이(가) 삭제 되었습니다.";
-        String text = simpleDateFormat.format(new Date()) + "에 해당 게시글이 삭제되었습니다.";
+        String text = dateTime.print(new DateTime()) + "에 해당 게시글이 삭제되었습니다.";
         try {
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage,"UTF-8"); // use multipart (true)
 
