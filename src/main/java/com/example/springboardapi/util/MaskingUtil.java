2764c54a46e492c91c2e7dc701c909126575d02b
jepark0121 <jepark0121@gmail.com>
2022-05-24 10:12:33 +0900

    fix: 마스킹 널처리, 공백제거 추가. 뭔가 고쳐 봤지만, 이게 과연 더 나은 방법일까 라는 생각이 든다.

--- src/main/java/com/example/springboardapi/util/MaskingUtil.java
+++ src/main/java/com/example/springboardapi/util/MaskingUtil.java
@@ -1,27 +1,32 @@
 package com.example.springboardapi.util;
 
 public class MaskingUtil {
+    /**
+     * 이름 마스킹 처리
+    */
     public static String nameMasking(String name) {
         String masked = "";
         String middleName = "";
 
+        if(name == null || name.trim().isEmpty()) {
+            return "";
+        }
+
         // 이름이 3자 이상이면
-        if(name.length() > 2) {
-            middleName = name.substring(1, name.length()-1);
-            System.out.println("middleName {}" + middleName);
+        if(name.trim().length() > 2) {
+            middleName = name.substring(1, name.trim().length()-1);
         } else {
             middleName = name.substring(1);
         }
 
-        String star = "";
+        masked = name.substring(0, 1);
         for(int i=0; i<middleName.length(); i++){
-            star += "*";
-        }
-
-        if(name.length() > 2) {
-            masked = name.substring(0, 1) + star + name.substring(name.length()-1, name.length());
-        } else {
-            masked = name.substring(0, 1) + star;
+            masked += "*";
+            if(i == middleName.length() - 1){
+                if(name.trim().length() > 2) {
+                    masked += name.substring(name.trim().length()-1, name.trim().length());
+                }
+            }
         }
 
         return masked;
