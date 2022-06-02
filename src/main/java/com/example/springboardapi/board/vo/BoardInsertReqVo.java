4d5ef2ab75ca4a42f393aff5484ed1ea16c79aa8
jepark0121 <jepark0121@gmail.com>
2022-06-02 16:43:24 +0900

    feat: NotBlank 로 변경 및 중복 데이터 유효성 추가.

--- src/main/java/com/example/springboardapi/board/vo/BoardInsertReqVo.java
+++ src/main/java/com/example/springboardapi/board/vo/BoardInsertReqVo.java
@@ -1,5 +1,7 @@
 package com.example.springboardapi.board.vo;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
@@ -11,12 +13,13 @@ import java.util.List;
 @NoArgsConstructor
 public class BoardInsertReqVo {
     private int boardId;
-    @NotEmpty(message = "VALID_NOT_NULL")
+    @NotBlank(message = "VALID_NOT_NULL")
     private String title;
-    @NotNull(message = "VALID_NOT_NULL")
-    @NotEmpty(message = "VALID_NOT_NULL")
+    @NotBlank(message = "VALID_NOT_NULL")
     @Size(max = 200, message = "LENGTH_ERROR")
     private String contents;
+    @UniqueElements(message = "DUPLICATE_ERROR")
+    @Size(max = 5, message = "MAX_SIZE_ERROR")
     private List<String> tagList;
     private String registId;
 
--- src/main/java/com/example/springboardapi/board/vo/BoardUpdateReqVo.java
+++ src/main/java/com/example/springboardapi/board/vo/BoardUpdateReqVo.java
@@ -1,14 +1,11 @@
 package com.example.springboardapi.board.vo;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.UniqueElements;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter
@@ -18,12 +15,13 @@ public class BoardUpdateReqVo {
     @Min(value = 1, message = "VALID_NOT_NULL")
     @NotNull(message = "VALID_NOT_NULL")
     private int boardId;
-    @NotEmpty(message = "VALID_NOT_NULL")
+    @NotBlank(message = "VALID_NOT_NULL")
     private String title;
-    @NotNull(message = "VALID_NOT_NULL")
-    @NotEmpty(message = "VALID_NOT_NULL")
+    @NotBlank(message = "VALID_NOT_NULL")
     @Size(max = 200, message = "LENGTH_ERROR")
     private String contents;
+    @UniqueElements(message = "DUPLICATE_ERROR")
+    @Size(max = 5, message = "MAX_SIZE_ERROR")
     private List<String> tagList;
     private String updateId;
 }
